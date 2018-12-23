7bc5f6138f25043a95f41c791e4ec380912d0c4a
zhangbaoli <82697585@qq.com>
2018-12-23 22:16:47 +0800

    update demo.py date

--- demo.py
+++ demo.py
@@ -8,21 +8,30 @@
 
 from selenium import webdriver
 import time
+import datetime
+
 driver = webdriver.Chrome()
 
-def getFirst(url,selector,keyword):
+def getFirst(url,selector,keyword,imgname):
     driver.get(url)
     if selector == 'kw':
         driver.find_element_by_id('kw').send_keys(keyword)
-    driver.save_screenshot('./20181223.png')
+    driver.save_screenshot('./'+imgname)
 
 url = 'http://www.baidu.com'
 key = 'Hello Word'
-getFirst(url,'kw',key)
+date = datetime.datetime.now()
+imgname = date.strftime('%Y%m%d%H%M%S') + '.png'
+getFirst(url,'kw',key,imgname)
 
 time.sleep(5)
 driver.quit()
 
-
-
+# import datetime as date
+# aa = date.datetime.now()
+# print(aa)
+# bb = date.datetime.now().strftime('%Y%m%d%H%M%S')
+# print(bb)
+# cc = date.datetime.now().strftime('%Y%m%d%H%M%S') + '.png'
+# print(cc)
 
