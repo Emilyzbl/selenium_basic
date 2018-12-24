6a9d1af673023d3e44514c6ff12ede4aa00f78af
zhangbaoli <82697585@qq.com>
2018-12-24 23:13:37 +0800

    add bing search

--- demo.py
+++ demo.py
@@ -12,10 +12,13 @@ import datetime
 
 driver = webdriver.Chrome()
 
+
 def getFirst(url,selector,keyword,imgname):
     driver.get(url)
     if selector == 'kw':
         driver.find_element_by_id('kw').send_keys(keyword)
+    elif selector == 'q':
+        driver.find_element_by_name(selector).send_keys(keyword)
     driver.save_screenshot('./'+imgname)
 
 url = 'http://www.baidu.com'
@@ -24,8 +27,14 @@ date = datetime.datetime.now()
 imgname = date.strftime('%Y%m%d%H%M%S') + '.png'
 getFirst(url,'kw',key,imgname)
 
-time.sleep(5)
+url1 = 'https://cn.bing.com'
+date1 = datetime.datetime.now()
+imgname1 = date1.strftime('%Y%m%d%H%M%S') + '.jpg'
+getFirst(url1,'q',key,imgname1)
+
+time.sleep(3)
 driver.quit()
+
 
 # import datetime as date
 # aa = date.datetime.now()
